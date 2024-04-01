6b91a93f8e5d26078268f9c80d0002bc4ea7c816
mdevboehm <mihail.boehm@gmail.com>
2024-04-01 14:53:32 +0200

    add logs

---
+++ src/logs/Logger.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace holybunch\shared\logs;
+
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\FirePHPHandler;
+use Monolog\Level;
+use Monolog\Processor\IntrospectionProcessor;
+
+class Logger extends \Monolog\Logger
+{
+    private const FILE_FORMAT = "%s/.logs/%s/log-%s-%s.log";
+
+    private function __construct(string $name,)
+    {
+        parent::__construct($name);
+    }
+
+    private static $instance = null;
+    public static function build(string $root, Level $level = Level::Info): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self("logger");
+
+            $handler = new StreamHandler(
+                sprintf(self::FILE_FORMAT, $root, date("Y"), date("m"), date("d")),
+                $level
+            );
+            $handler->setFormatter(new LineFormatter(
+                "[%datetime%] [%level_name%]: %message% %context% \n",
+                'Y-m-d H:i:s',
+                false,
+                true
+            ));
+
+            self::$instance->pushHandler($handler);
+            self::$instance->pushHandler(new FirePHPHandler());
+            self::$instance->pushProcessor(new IntrospectionProcessor(Level::Info, array(), 1));
+        }
+        return  self::$instance;
+    }
+}
---
+++ tests/logs/LoggerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace holybunch\shared\tests\logs;
+
+use holybunch\shared\tests\BaseTest;
+use holybunch\shared\logs\Logger;
+use Monolog\Level;
+
+final class LoggerTest extends BaseTest
+{
+    private string $file = "tests/.tmp/.logs/%s/log-%s-%s.log";
+    private Logger $logger;
+
+    protected function tearDown(): void
+    {
+        unlink(sprintf($this->file, date("Y"), date("m"), date("d")));
+    }
+
+    public function testDebugHappy(): void
+    {
+        $this->logger = Logger::build("tests/.tmp/", Level::Debug);
+        $this->logger->debug("message", ["type" => "debug"]);
+        $this->logger->info("message", ["type" => "info"]);
+        $this->logger->warning("message", ["type" => "warning"]);
+        $this->logger->error("message", ["type" => "error"]);
+
+        $logContents = file_get_contents(
+            sprintf($this->file, date("Y"), date("m"), date("d"))
+        );
+        $this->assertStringContainsString('{"type":"debug"}', $logContents);
+        $this->assertStringContainsString('{"type":"info"}', $logContents);
+        $this->assertStringContainsString('{"type":"warning"}', $logContents);
+        $this->assertStringContainsString('{"type":"error"}', $logContents);
+    }
+}
