0731afd06a586c3472d54626c70f46380b42827c
mihailboehm <mihail.boehm@sap.com>
2024-04-04 14:55:20 +0200

    music part 1

---
+++ src/media/music/Service.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace holybunch\shared\media\music;
+
+use Exception;
+use holybunch\shared\exceptions\SharedException;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use SplFileInfo;
+use UnexpectedValueException;
+
+class Service
+{
+    /** @var string[] */
+    private array $exclusions;
+
+    /** @return string[] */
+    public function exclusions(): array
+    {
+        return $this->exclusions;
+    }
+
+    private string $path;
+
+    public function __construct(string $path)
+    {
+        $this->path = $path;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function collectionSongs(string $collection): array
+    {
+        try {
+            $songs = [];
+            $this->exclusions = [];
+            $rdi = new RecursiveDirectoryIterator($this->path . $collection);
+            foreach (new RecursiveIteratorIterator($rdi) as $file) {
+                if ($file instanceof SplFileInfo) {
+                    $this->processMusicFile($file, $collection, $songs);
+                }
+            }
+            return $songs;
+        } catch (UnexpectedValueException $e) {
+            throw new SharedException($e);
+        }
+    }
+
+    /**
+     * @param string[] $songs
+     */
+    private function processMusicFile(SplFileInfo $file, string $collection, array &$songs): void
+    {
+        try {
+            if ($file->isFile()) {
+                $songs[] = Entity::parse($file->getPathname());
+            }
+        } catch (Exception $e) {
+            $this->exclusions[] = "error for collection '$collection' and file '$file' occurred: " . $e->getMessage();
+        }
+    }
+}
